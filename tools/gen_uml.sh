719d60a2ddb8a3c408a893c1cfd2755e631d232d
Mark Fisher <mark.j.fisher@gmail.com>
2025-12-09 15:01:40 +0000

    Update generated UML diagrams

--- tools/gen_uml.sh
+++ tools/gen_uml.sh
@@ -8,9 +8,16 @@ date
 BUILD_TARGET=fujibus-pty-debug
 
 ./build.sh -cp ${BUILD_TARGET}
-clang-uml -c clang-uml.yml -d build/${BUILD_TARGET}
 
 mkdir -p docs/images
+mkdir -p docs/uml
+
+# remove old files in case their UML is removed.
+rm -f docs/images/gen_*.svg
+rm -f docs/uml/gen_*.puml
+
+# generate new versions
+clang-uml -c clang-uml.yml -d build/${BUILD_TARGET}
 plantuml -v --duration -tsvg docs/uml/*.puml -o ../images
 
 echo "Done generating UML diagrams."
