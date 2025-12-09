f83e5ccbc26267a8718bccf3176e6bc7c9d44d0c
Mark Fisher <mark.j.fisher@gmail.com>
2025-12-09 13:39:20 +0000

    Make gen_uml.sh use build.sh and give it fujibus-pty-debug as build target

--- tools/gen_uml.sh
+++ tools/gen_uml.sh
@@ -5,17 +5,9 @@ set -x
 echo "Generating UML diagrams..."
 date
 
-# 1. Ensure build dir + compile_commands.json exist
-mkdir -p build
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build build -j
-
-# 2. Run clang-uml using your config
+./build.sh -cp fujibus-pty-debug
 clang-uml -c clang-uml.yml
 
-# 3. Render all generated .puml to SVG
-#    Note: output_directory in clang-uml.yml is docs/uml,
-#          so we render those into docs/images.
 mkdir -p docs/images
 plantuml -v --duration -tsvg docs/uml/*.puml -o ../images
 
