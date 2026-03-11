22356e656116ced1a2b992f71a7a392a7a3f697b
Mark Fisher <mark.j.fisher@gmail.com>
2026-03-11 20:27:47 +0000

    Debug up to 512 bytes

--- include/fujinet/core/utils.h
+++ include/fujinet/core/utils.h
@@ -9,6 +9,6 @@ void format_hex_prefix(const std::uint8_t* buffer, std::size_t len, char* out, s
 
 // Debug: hexdump bytes to log (16 per line, hex + ASCII). No-op when FN_DEBUG is not set.
 // tag: log tag for FN_LOGI; max_bytes: cap at 256 by default.
-void log_hexdump(const char* tag, const std::uint8_t* data, std::size_t size, std::size_t max_bytes = 256);
+void log_hexdump(const char* tag, const std::uint8_t* data, std::size_t size, std::size_t max_bytes = 512);
 
 }  // namespace fujinet::core
